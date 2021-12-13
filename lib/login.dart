ced3600dfff4c8565ab49b2b1123c4c287e194eb
Greg Holmes <greg.holmes@tru.id>
2021-12-13 19:57:00 +0000

    Removed parts of libraries due to these not being included until later in the tutorial

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,4 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:flutter_appauth/flutter_appauth.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
