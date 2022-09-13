7545d41806b69100f62b61de0b81859e235934c7
Greg Holmes <greg.holmes@tru.id>
2022-09-13 15:33:59 +0100

    Removed unused .env and unneccessary usage of Containers

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_appauth/flutter_appauth.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 const String baseURL = '<YOUR-NGROK-URL>';
 
@@ -17,29 +16,27 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: ListView(
-          children: [
-            Container(
-                padding: const EdgeInsets.only(bottom: 45.0),
-                margin: const EdgeInsets.only(top: 40),
-                child: Image.asset(
-                  'assets/images/tru-id-logo.png',
-                  height: 100,
-                )),
-            Container(
-                width: double.infinity,
-                margin: const EdgeInsets.only(bottom: 20),
-                child: const Text(
-                  'Login.',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
-                  ),
-                )),
-          ],
-        ),
+      body: ListView(
+        children: [
+          Container(
+              padding: const EdgeInsets.only(bottom: 45.0),
+              margin: const EdgeInsets.only(top: 40),
+              child: Image.asset(
+                'assets/images/tru-id-logo.png',
+                height: 100,
+              )),
+          Container(
+              width: double.infinity,
+              margin: const EdgeInsets.only(bottom: 20),
+              child: const Text(
+                'Login.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              )),
+        ],
       ),
     );
   }
